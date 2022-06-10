3b7e7a22d69c8660b8988e3fdd95a9ce52a928de
Steffen Schmidt <git@brombeersaft.com>
2022-06-11 01:21:33 +0200

    feat: first working version

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import * as core from "@actions/core";
 import os from "os";
+import { getGenesyscloudArchy } from "./installer";
 
 export async function run(): Promise<void> {
 	try {
+		// Gater GitHub Actions inputs
 		const version: string = core.getInput("archy-version");
-		const arch = os.arch();
+
+		// Gather OS details
+		const osPlatform = os.platform();
+
+		getGenesyscloudArchy(version, osPlatform);
 	} catch (error) {
 		if (error instanceof Error) {
 			core.setFailed(error.message);
