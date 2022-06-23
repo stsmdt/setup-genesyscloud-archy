d0029bbaeb73fc9195ca2e6a615c0a79af448907
Steffen Schmidt <git@brombeersaft.com>
2022-06-23 13:19:59 +0200

    feat: add possibility to provide latest version

--- src/installer.ts
+++ src/installer.ts
@@ -1,13 +1,33 @@
 import * as core from "@actions/core";
+import * as hc from "@actions/http-client";
 import * as io from "@actions/io";
 import * as tc from "@actions/tool-cache";
+import * as semver from "semver";
 import path from "path";
 import os from "os";
+
+interface IArchyVersion {
+	version: string;
+}
 
 export async function getGenesyscloudArchy(
 	version: string,
 	osPlatform: string = os.arch()
 ) {
+	const genesyscloudArchyVersions = await getVersions();
+
+	if (isLatestSyntax(version)) {
+		version = genesyscloudArchyVersions[0].version;
+		core.info("Getting latest genesyscloud archy version...");
+	} else {
+		if (
+			genesyscloudArchyVersions.filter((value) => value.version == version)
+				.length == 0
+		) {
+			throw new Error(`Unable to find genesyscloud archy version ${version}`);
+		}
+	}
+
 	const platform = mapOS(osPlatform);
 
 	let toolPath: string;
@@ -45,11 +65,30 @@ export async function getGenesyscloudArchy(
 	core.addPath(toolPath);
 }
 
-function mapOS(os: string) {
+export async function getVersions(): Promise<IArchyVersion[]> {
+	const dataUrl = "https://sdk-cdn.mypurecloud.com/archy/versions.json";
+	const httpClient = new hc.HttpClient("setup-genesyscloud-archy", [], {
+		allowRetries: true,
+		maxRetries: 3,
+	});
+	const response = await httpClient.getJson<IArchyVersion[]>(dataUrl);
+	let versions: IArchyVersion[] = [];
+	if (response.result) {
+		versions = response.result;
+		versions.sort((v1, v2) => semver.rcompare(v1.version, v2.version));
+	}
+	return versions;
+}
+
+function mapOS(os: string): string {
 	const mappings = {
 		win32: "win",
 		darwin: "macos",
 	};
 
 	return mappings?.[os as keyof typeof mappings] || os;
+}
+
+function isLatestSyntax(versionSpec: string): boolean {
+	return ["current", "latest"].includes(versionSpec);
 }
